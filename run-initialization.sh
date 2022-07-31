e12475e0cd54690ea0ec34101ab51d754cadf83e
Raphael Coelho <rmdcoelho@gmail.com>
2022-07-31 00:19:44 -0300

    Create file to mark db as created

--- run-initialization.sh
+++ run-initialization.sh
@@ -1,10 +1,16 @@
+
+if [ ! -e /db-started ]
+then
 # Wait to be sure that SQL Server came up
-sleep 90s
+    sleep @TIMEOUT
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-for f in ./scripts/*
-    do
+    for f in ./scripts/*.sql
+        do
         echo "$f"
         /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P SqlServer2022* -d master -i $f
-done
+    done && touch /db-started
+else
+    echo "Database already setup"
+fi
